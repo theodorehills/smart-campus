086e24e163207d637ae31569c89f2a58a34dcde5
theod <theodore.hills@outlook.com>
2022-08-05 21:37:25 +0800

    feat: mark some fields as TableField

--- src/main/java/cn/vorbote/smartcampus/pos/BasePo.java
+++ src/main/java/cn/vorbote/smartcampus/pos/BasePo.java
@@ -1,8 +1,11 @@
 package cn.vorbote.smartcampus.pos;
 
-import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
 /**
  * BasePo<br>
@@ -12,16 +15,23 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@Accessors(chain = true)
 public sealed abstract class BasePo permits Admin, Grade, Klasse, Student, Teacher {
 
+    @TableField(value = "create_by", fill = FieldFill.INSERT)
     protected String createBy;
 
+    @TableField(value = "create_at", fill = FieldFill.INSERT)
     protected Long createAt;
 
+    @TableField(value = "update_by", fill = FieldFill.INSERT)
     protected String updateBy;
 
+    @TableField(value = "update_at", fill = FieldFill.INSERT)
     protected Long updateAt;
 
+    @TableField(value = "archived", fill = FieldFill.INSERT)
+    @TableLogic
     protected Integer archived;
 
 }
