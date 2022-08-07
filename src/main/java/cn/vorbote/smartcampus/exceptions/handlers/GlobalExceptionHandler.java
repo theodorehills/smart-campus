1c72c2905b4c578f658aad1c9581608708a0940c
theod <theodore.hills@outlook.com>
2022-08-07 23:55:32 +0800

    fix: 解决Token过期会直接抛出异常的问题

--- src/main/java/cn/vorbote/smartcampus/exceptions/handlers/GlobalExceptionHandler.java
+++ src/main/java/cn/vorbote/smartcampus/exceptions/handlers/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import cn.vorbote.smartcampus.exceptions.NotImplementedException;
 import cn.vorbote.web.constants.WebStatus;
 import cn.vorbote.web.exceptions.BizException;
 import cn.vorbote.web.model.ResponseResult;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -30,5 +31,12 @@ public class GlobalExceptionHandler {
     @ResponseBody
     public ResponseResult<?> handle(BizException ex) {
         return ex.respond();
+    }
+
+    @ExceptionHandler(TokenExpiredException.class)
+    @ResponseBody
+    public ResponseResult<?> handle(TokenExpiredException ex) {
+        return ResponseResult.error("您的认证信息已过期，请重新登录！")
+                .code(WebStatus.PAYMENT_REQUIRED);
     }
 }
