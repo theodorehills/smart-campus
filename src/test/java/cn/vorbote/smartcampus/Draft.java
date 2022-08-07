578c52668833cc9b69b394d7637471db324e0f82
theod <theodore.hills@outlook.com>
2022-08-07 08:38:22 +0800

    test: added a draft

---
+++ src/test/java/cn/vorbote/smartcampus/Draft.java
@@ -0,0 +1,27 @@
+package cn.vorbote.smartcampus;
+
+import cn.vorbote.core.utils.SnowFlake;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Draft<br>
+ * Created at 8/7/2022 7:57 AM
+ *
+ * @author theod
+ */
+@Slf4j
+@SpringBootTest
+public class Draft {
+
+    @Autowired
+    private SnowFlake snowFlake;
+
+    @Test
+    void ctxLoads() {
+        log.info("{}", String.valueOf(snowFlake.nextId()).length());
+    }
+
+}
